86bf12bb189a882b52efe2eee50319f121ff77cf
Ferris Lucas <ferris.lucas@gmail.com>
2012-01-26 00:57:22 -0600

    create InputModelAggregateRoot

---
+++ src/InputModelAggregateRoot/InputModelAggregateRoot.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimpleCqrs.Domain;
+using SimpleCqrs.Eventing;
+
+namespace InputModelAggregateRoot
+{
+    public class InputModelAggregateRoot<T> : AggregateRoot
+    {
+        public InputModelAggregateRoot(Guid aggregateRootId)
+		{
+			Id = aggregateRootId;
+			Apply(new InputModelAggregateRootCreatedEvent(){ AggregateRootId = aggregateRootId });
+		}
+
+        public InputModelAggregateRoot()
+		{
+		}
+
+        protected void OnInputModelAggregateRootCreated(InputModelAggregateRootCreatedEvent inputModelAggregateRootCreatedEvent)
+        {
+            Id = inputModelAggregateRootCreatedEvent.AggregateRootId;
+        }
+
+        public void SubmitInputModel(T inputModel, string securityInformation)
+        {
+            Apply(new InputModelSubmittedEvent<T>()
+                      {
+                          InputModelType = inputModel.GetType(),
+                          SecurityInformation = securityInformation,
+                          InputModel = inputModel,
+                      });
+        }
+
+    }
+
+    public class InputModelSubmittedEvent<T> : DomainEvent
+    {
+        public T InputModel { get; set; }
+
+        public Type InputModelType { get; set; }
+
+        public string SecurityInformation { get; set; }
+    }
+
+    public class InputModelAggregateRootCreatedEvent : DomainEvent
+    {
+    }
+}
---
+++ src/InputModelAggregateRoot/SubmitInputModelCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimpleCqrs.Commanding;
+using SimpleCqrs.Domain;
+
+namespace InputModelAggregateRoot
+{
+    public class SubmitInputModelCommand<T> : CommandWithAggregateRootId
+    {
+        public T InputModel { get; set; }
+
+        public string SecurityInformation { get; set; }
+    }
+
+    public class SubmitInputModelCommandHandler<T> : CommandHandler<SubmitInputModelCommand<T>>
+    {
+        private readonly IDomainRepository domainRepository;
+
+        public SubmitInputModelCommandHandler(IDomainRepository domainRepository)
+        {
+            this.domainRepository = domainRepository;
+        }
+
+        public override void Handle(SubmitInputModelCommand<T> command)
+        {
+            var inputModelAggregateRoot = new InputModelAggregateRoot<T>(Guid.NewGuid());
+            inputModelAggregateRoot.SubmitInputModel(command.InputModel, command.SecurityInformation);
+            domainRepository.Save(inputModelAggregateRoot);
+        }
+    }
+}
